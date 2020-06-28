04ea7e7fef4175d467e3a8367c001ccd52614cc2
gai <gaih@outlook.com>
2020-06-29 00:39:41 +0800

    快排

--- dictSort.py
+++ dictSort.py
@@ -2,21 +2,22 @@
 # 基于快排的实现
 
 origin_str = "abcba"
-sort_list = []
-end = len(origin_str)
+sort_list = ["abcba","bcba","cba","ba","a"]
+end = len(origin_str)-1
 start = 0
 
-def quick_sort(origin_str,sort_list,start,end):
+def quick_sort(sort_list,start,end):
+    print(sort_list)
     while(start < end):
-        i = partition(sort_list,origin_str,start,end)
-        quick_sort(sort_list,origin_str,start,end-1)
-        start+=1
+        i = partition(sort_list,start,end)
+        quick_sort(sort_list,start,i-1)
+        start=i+1
+    print(sort_list)
     return
 
-def partition(sort_list,origin_str,start,end):
-    _tmp = sort_list[start]
-    tmp = str(sort_list[start])
-    while(left < end):
+def partition(sort_list,start,end):
+    tmp = sort_list[start]
+    while(start< end):
         while(start<end and sort_list[end]>tmp):
             end-=1
         if start<end:
@@ -26,8 +27,8 @@ def partition(sort_list,origin_str,start,end):
         if start < end:
             sort_list[end] = sort_list[start]
             end-=1
-    sort_list[start] = _tmp
+    sort_list[start] = tmp
     return start
 
 
-quick_sort(origin_str,sort_list,start,end)
+quick_sort(sort_list,start,end)

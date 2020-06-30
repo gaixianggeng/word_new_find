01d2bfca1156d98dd2de19756af8e2b09989d23c
gai <gaih@outlook.com>
2020-07-01 02:10:41 +0800

    添加CMake编译

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 // C++版本实现快排
-
+#include "wordSort.h"
 #include <iostream>
+#include <vector>
 using namespace std;
 int main(){
 		cout<<"hello world!"<<endl;
+		vector<string> test = {"test"};
+		WordSort ws;
+		ws.quickSort(test, 0, 8);
 }
---
+++ wordSort.cpp
@@ -0,0 +1,18 @@
+//实现标题数据抽词排序
+
+#include "wordSort.h"
+
+vector<string> WordSort::quickSort(vector<string>,int start,int end){
+		vector<string> res;
+		cout<<"word sort"<<endl;
+		return res;
+
+}
+vector<string> WordSort::string2Arr(string title){
+
+		vector<string> res;
+		cout<<"string2Arr"<<endl;
+		return res;
+}
+
+
